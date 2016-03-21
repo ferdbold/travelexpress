bc7865675f9f3c80e205798652941908cab4573a
Frédéric Bolduc <bolducfrederic@gmail.com>
2016-03-21 14:04:02 -0400

    Login and navigation template

--- public/urls.py
+++ public/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls import url
 from . import views
 
 app_name = 'public'
+
 urlpatterns = [
+	url(r'^connexion/$', views.login, name='login'),
 	url(r'^$', views.index, name='index')
 ]
--- public/views.py
+++ public/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
 	return render(request, 'public/index.html', {})
+
+def login(request):
+	return render(request, 'public/login.html', {})
