64516f571d97de4ab1f9a97b14c512ce7ec40de9
Frédéric Bolduc <bolducfrederic@gmail.com>
2016-03-31 13:01:32 -0400

    User registration

--- public/views.py
+++ public/views.py
@@ -1,25 +1,26 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 
 def index_view(request):
 	return render(request, 'public/index.html', {})
 
 def login_view(request):
-	if (request.method == 'POST'):
-		user = authenticate( \
-			username = request.POST['username'], \
-			password = request.POST['password'] \
+	if request.method == 'POST':
+		user = authenticate(
+			username = request.POST['username'],
+			password = request.POST['password']
 		)
 
 		if user is not None:
 			login(request, user)
 			return redirect('public:index')
 		else:
-			return render(request, 'public/login.html', { \
-				'alert': { \
-					'type': 'danger', \
-					'message': 'Erreur de connexion. Veuillez réessayer' \
-				} \
+			return render(request, 'public/login.html', {
+				'alert': {
+					'type': 'danger',
+					'message': 'Erreur de connexion. Veuillez réessayer'
+				}
 			})
 
 	# GET requests
@@ -31,4 +32,33 @@ def logout_view(request):
 	return redirect('public:index')
 
 def register_view(request):
-	return render(request, 'public/register.html', {})
+	if request.method == 'POST':
+		user = User.objects.get(username=request.POST['username'])
+		if user is not None:
+			return render(request, 'public/register.html', {
+				'alert': {
+					'type': 'danger',
+					'message': 'Ce nom d\'utilisateur est déjà utilisé.'
+				}
+			})
+
+		user = User.objects.get(email=request.POST['email'])
+		if user is not None:
+			return render(request, 'public/register.html', {
+				'alert': {
+					'type': 'danger',
+					'message': 'Cette adresse courriel est déjà utilisée.'
+				}
+			})
+
+		if request.POST['password'] != request.POST['password_validation']:
+			return render(request, 'public/register.html', {
+				'alert': {
+					'type': 'danger',
+					'message': 'Les deux mots de passe n\'étaient pas identiques.'
+				}
+			})
+
+	# GET requests
+	else:
+		return render(request, 'public/register.html', {})
