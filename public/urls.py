632f9af00acad587e56a90fa68da427ff61c1181
Frédéric Bolduc <bolducfrederic@gmail.com>
2016-04-01 00:35:21 -0400

    Renamed URLs to English

--- public/urls.py
+++ public/urls.py
@@ -5,10 +5,10 @@ from . import views
 app_name = 'public'
 
 urlpatterns = [
-	url(r'^connexion/$', views.LoginView.as_view(), name='login'),
-	url(r'^deconnexion/$', views.LogoutView.as_view(), name='logout'),
-	url(r'^inscription/$', views.RegisterView.as_view(), name='register'),
-	url(r'^voyages/creer/$', views.TripCreateView.as_view(), name='trip_create'),
-	url(r'^voyages/(?P<pk>[0-9]+)/$', views.TripDetailView.as_view(), name='trip_detail'),
+	url(r'^login/$', views.LoginView.as_view(), name='login'),
+	url(r'^logout/$', views.LogoutView.as_view(), name='logout'),
+	url(r'^register/$', views.RegisterView.as_view(), name='register'),
+	url(r'^trips/new/$', views.TripCreateView.as_view(), name='trip_create'),
+	url(r'^trips/(?P<pk>[0-9]+)/$', views.TripDetailView.as_view(), name='trip_detail'),
 	url(r'^$', views.IndexView.as_view(), name='index')
 ]
