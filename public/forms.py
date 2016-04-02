6e8ef7edb44c976ea253f15c6d5ff4960af91eed
Frédéric Bolduc <bolducfrederic@gmail.com>
2016-04-01 20:55:09 -0400

    Preferences form
    - Added new preferences view/form to edit user account settings
    - Added tolerates/does_not_tolerate fields to user profile

--- public/forms.py
+++ public/forms.py
@@ -2,13 +2,14 @@ from django import forms
 from django.contrib.auth.models import User
 
 class RegisterForm(forms.Form):
+	"""Handles new user regisration"""
 	first_name = forms.CharField(max_length=100)
 	last_name = forms.CharField(max_length=100)
 	email = forms.EmailField(max_length=100)
 	username = forms.CharField(max_length=50)
 	password = forms.CharField(max_length=50, widget=forms.PasswordInput)
 	password_validation = forms.CharField(label='Password (again)', max_length=50, widget=forms.PasswordInput)
-	phone_number = forms.CharField(max_length=10)
+	phone = forms.CharField(max_length=10)
 
 	def clean(self):
 		cleaned_data = super(RegisterForm, self).clean()
@@ -38,3 +39,14 @@ class RegisterForm(forms.Form):
 			self.add_error('password_validation', msg)
 
 		return cleaned_data
+
+class PreferencesForm(forms.Form):
+	"""Handles user preferences"""
+	first_name = forms.CharField(max_length=100)
+	last_name = forms.CharField(max_length=100)
+	email = forms.EmailField(max_length=100)
+	phone = forms.CharField(max_length=10)
+	tolerates = forms.CharField(max_length=500, widget=forms.Textarea)
+	does_not_tolerate = forms.CharField(max_length=500, widget=forms.Textarea)
+
+	# TODO: Validate email to prevent people from taking an already existing email through this form
